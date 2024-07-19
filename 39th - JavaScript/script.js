fff090101c24fed15703c955ee5dc957d5cfd75a
Manav Verma <manavvrma17@gmail.com>
2024-07-20 00:33:08 +0530

    day 12

---
+++ 39th - JavaScript/script.js
@@ -0,0 +1,23 @@
+alert("Your Laptop will Explode!");
+
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+// console.log("Hello World");
+
+
+var a = prompt("Enter your name");
+var isTrue = confirm("are you sure you want to leave this page and blast your computer?")
+
+if (isTrue){
+    console.log("Computer left!");
+}
+
+
+else{
+    console.log("Computer safe!");
+}
+
+console.log(2+2);
+
+ 
---
+++ 40th_Variables/script.js
@@ -0,0 +1,14 @@
+console.log("Hey this is starting")
+
+var a = 5;
+var b = 6;
+var c = "Manav";
+var $a = 2;
+
+
+
+// cant use nu,bers in start, can start with underscore and $
+
+console.log(a+b+c+$a);
+console.log(a+b);
+console.log(typeof a,typeof b, typeof c);
