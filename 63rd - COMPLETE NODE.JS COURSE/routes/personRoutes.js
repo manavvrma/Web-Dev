bdf90517b3549d0c2f9e9f85154d9ef6a4b08a89
Manav Verma <manavvrma17@gmail.com>
2024-11-06 13:36:53 +0530

    update 23

--- 63rd - COMPLETE NODE.JS COURSE/routes/personRoutes.js
+++ 63rd - COMPLETE NODE.JS COURSE/routes/personRoutes.js
@@ -22,7 +22,7 @@ router.post("/", async (req, res) => {
 router.get("/", async (req, res) => {
   try {
     const data = await Person.find();
-    console.log("data found");
+    console.log("Person data found");
     res.status(200).json(data);
   } catch (err) {
     console.log(err);
@@ -39,11 +39,52 @@ router.get("/:workType", async (req, res) => {
       workType === "manager"
     ) {
       const response = await Person.find({ work: workType });
-      console.log("worktype found");
+      console.log("work type found");
       res.status(200).json(response);
     } else {
       res.status(400).json({ error: "Invalid work type" });
     }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    const personId = req.params.id; // Extract personId from the URL parameter
+    const updatedPersonData = req.body; //
+
+    const response = await Person.findByIdAndUpdate(
+      personId,
+      updatedPersonData,
+      {
+        new: true, // Return the updated person document
+        runValidators: true, // Run the mongoose validators
+      }
+    );
+
+    if (!response) {
+      return res.status(404).json({ error: "Person not found" });
+    }
+
+    console.log("person data updated");
+    res.status(200).json(response);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const personId = req.params.id; // Extract personId from the URL parameter
+    const response = await Person.findByIdAndDelete(personId);
+    if (!response) {
+      return res.status(404).json({ error: "Person not found" });
+    }
+    console.log("person data deleted");
+    res.status(200).json(response);
   } catch (err) {
     console.log(err);
     res.status(500).json({ error: "Internal Server Error" });
--- 63rd - COMPLETE NODE.JS COURSE/server.js
+++ 63rd - COMPLETE NODE.JS COURSE/server.js
@@ -6,7 +6,8 @@ const db = require("./db.js");
 const bodyParser = require("body-parser");
 app.use(bodyParser.json()); // req.body
 
-const MenuItem = require("./models/MenuItem.js");
+const personRoutes = require("./routes/personRoutes.js"); // Import personRoutes
+const menuRoutes = require("./routes/menuRoutes.js"); // Import menuRoutes
 
 app.get("/", function (req, res) {
   res.send("Welcom to my website!");
